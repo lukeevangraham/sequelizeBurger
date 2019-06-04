098c52d4c6338723ebfe3acbcf9be7c79d180bf5
Cefalu Wave <luke.evan.graham@gmail.com>
2019-06-03 21:37:20 -0700

    adding initial files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chili Cheese Burger", false);
