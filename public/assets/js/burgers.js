e5ddf781c6cb8d7bebee6ab62485fff80e6d86e1
Cefalu Wave <luke.evan.graham@gmail.com>
2019-06-08 15:04:04 -0700

    working on routing

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,29 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
+(document).ready(function () {
+    // Getting a reference to the input field where user adds a new burger
+    var $newItemInput = $("#ca");
+
+    // Adding event listeners to the form to create a new object, and the button to delete
+    // an Author
+    $(document).on("submit", "#burger-form", handleAuthorFormSubmit);
+    $(document).on("click", ".change-devour", handleDeleteButtonPress);
+
+    // Getting the initial list of Authors
+    getBurgers();
+
+
+
+    // This function grabs todos from the database and updates the view
+    function getBurgers() {
+        $.get("/api/burgers", function (data) {
+            burgers = data;
+            initializeRows();
+        });
+    }
+
+
+
+    // Make sure we wait to attach our handlers until the DOM is fully loaded.
+    // $(function () {
     $(".change-devour").on("click", function (event) {
         var id = $(this).data("id");
         // var newDevour = $(this).data("newdevour");
@@ -21,49 +45,42 @@ $(function () {
         );
     });
 
-    $(".create-form").on("submit", function (event) {
-        //     // Make sure to preventDefault on a submit event.
-        //     event.preventDefault();
+    // $(".create-form").on("submit", function (event) {
 
-        //     var newBurger = {
-        //         burger_name: $("#ca").val().trim(),
-        //         devoured: 0
-        //     };
+    // Make sure to preventDefault on a submit event.
+    // event.preventDefault();
 
-        //     // Send the POST request.
-        //     $.ajax("/api/burgers", {
-        //         type: "POST",
-        //         data: newBurger
-        //     }).then(
-        //         function () {
-        //             console.log("created new burger");
-        //             // Reload the page to get the updated list
-        //             location.reload();
-        //         }
-        //     );
+    // var newBurger = {
+    //     burger_name: $("#ca").val().trim(),
+    //     devoured: "false"
+    // };
 
-        // Make sure to preventDefault on a submit event.
+    // // Send the POST request.
+    // $.ajax("/api/burgers", {
+    //     type: "POST",
+    //     data: newBurger
+    // }).then(
+    //     function () {
+    //         console.log("created new burger");
+    //         // Reload the page to get the updated list
+    //         location.reload();
+    //     }
+    // );
+
+
+
+
+    // });
+
+    function insertTodo(event) {
         event.preventDefault();
-
-        var newBurger = {
-            burger_name: $("#ca").val().trim(),
-            devoured: "false"
+        var burger = {
+            burger_name: $newItemInput.val().trim(),
+            devoured: false
         };
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("created new burger");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
+        $.post("/api/todos", burger, getBurgers);
+        $newItemInput.val("");
+    }
 
-
-
-
-    });
 });
