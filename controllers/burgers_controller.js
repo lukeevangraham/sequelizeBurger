bbdb5a481b98deb7110837f0b56d00596ff6498c
Cefalu Wave <luke.evan.graham@gmail.com>
2020-03-04 21:19:23 -0800

    devoured button works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,21 +7,20 @@ var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  db.Burger.findAll({
-  }).then(function(data) {
-    let newData = []
+  db.Burger.findAll({}).then(function(data) {
+    let newData = [];
     data.forEach(burger => {
-      newData.push(burger.dataValues)
+      newData.push(burger.dataValues);
     });
     var hbsObject = {
       burgers: newData
     };
     res.render("index", hbsObject);
-  })
+  });
 });
 
 router.post("/api/burgers", function(req, res) {
-  console.log("INSERTING: ", req.body.burger_name)
+  console.log("INSERTING: ", req.body.burger_name);
   // console.log('devoured', req.body.devoured)
   // burger.insertOne([
   //   "burger_name", "devoured"
@@ -37,29 +36,35 @@ router.post("/api/burgers", function(req, res) {
     burger_name: req.body.burger_name,
     devoured: 0
   }).then(function(result) {
-    res.json(result)
-  })
+    res.json(result);
+  });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  // console.log("condition", condition);
+  let devouredStatus = { devoured: "1" };
 
-  // console.log("the body is", req.body.devoured);
-
-  db.updateOne({
-    // devoured: req.body.devoured
-    devoured: 1
-  }, condition, function(result) {
-    // console.log(result);
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+  db.Burger.update(devouredStatus, {
+    where: {
+      id: req.params.id
     }
+  }).then(function(result) {
+    res.json(result);
+    console.log("RES: ", result)
   });
+  // }{
+  // devoured: req.body.devoured
+  // devoured: 1
+  // }, condition, function(result) {
+  // console.log(result);
+  // if (result.changedRows == 0) {
+  // If no rows were changed, then the ID must not exist, so 404
+  // return res.status(404).end();
+  // } else {
+  // res.status(200).end();
+  // }
+  // });
 });
 
 // Export routes for server.js to use.
